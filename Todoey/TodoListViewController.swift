3e5f3aa9e23111744e381cf7acf698e6f4b25fcc
Alexandre Giannetto <32279162+Alexg78bis@users.noreply.github.com>
2020-10-30 16:37:13 +0100

    wip

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray = ["Find Mike", "Buy eggs", "Destroy the demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - TableView Datasource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = cell?.accessoryType == .checkmark ? .none : .checkmark
+    }
+    
+    //MARK: - Add new item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add new todoey item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textfield = alertTextField
+        }
+        
+        alert.addAction(UIAlertAction(title: "Add item", style: .default) { (action) in
+            if let textValue = textfield.text{
+                self.itemArray.append(textValue)
+                self.tableView.reloadData()
+            }
+        })
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+}
+
